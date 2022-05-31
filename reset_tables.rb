48ea2871ac0fe41b496932836436e62ef2322d86
Tom <T.A.Rose1993@gmail.com>
2022-05-31 15:53:42 +0100

    Work so far, mostly front end

--- reset_tables.rb
+++ reset_tables.rb
@@ -3,10 +3,10 @@ require "database_connection"
 
 def reset_tables(db)
   db.run("DROP TABLE IF EXISTS client;")
-  db.run("CREATE TABLE client (id SERIAL PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL, contact TEXT NOT NULL, email TEXT NOT NULL);")
+  db.run("CREATE TABLE client (id SERIAL PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL, name TEXT NOT NULL, contact_number TEXT NOT NULL, email TEXT NOT NULL);")
 
   db.run("DROP TABLE IF EXISTS properties;")
-  db.run("CREATE TABLE properties (id SERIAL PRIMARY KEY, property_name TEXT NOT NULL, description TEXT NULL, price FLOAT NOT NULL, availablity_start DATE, availablity_end DATE")
+  db.run("CREATE TABLE properties (id SERIAL PRIMARY KEY, property_name TEXT NOT NULL, description TEXT NULL, price FLOAT NOT NULL, availablity_start TEXT NOT NULL, availablity_end TEXT NOT NULL")
 
 end
 
