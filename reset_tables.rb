4ffea0730184cddab6b2824f5de3fb09c89a8104
Endre Barabas <barabasandrew@gmail.com>
2022-06-01 12:28:23 +0100

    Users database

--- reset_tables.rb
+++ reset_tables.rb
@@ -3,10 +3,32 @@ require "database_connection"
 
 def reset_tables(db)
   db.run("DROP TABLE IF EXISTS client;")
-  db.run("CREATE TABLE client (id SERIAL PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL, name TEXT NOT NULL, contact_number TEXT NOT NULL, email TEXT NOT NULL);")
+  db.run("CREATE TABLE client (id SERIAL PRIMARY KEY,
+   username TEXT NOT NULL, 
+   password TEXT NOT NULL, 
+   name TEXT NOT NULL, 
+   contact_number TEXT NOT NULL, 
+   email TEXT NOT NULL
+   );")
 
   db.run("DROP TABLE IF EXISTS properties;")
-  db.run("CREATE TABLE properties (id SERIAL PRIMARY KEY, property_name TEXT NOT NULL, description TEXT NOT NULL, price FLOAT NOT NULL, availability_start DATE NOT NULL, availability_end DATE NOT NULL);")
+  db.run("CREATE TABLE properties (
+    id SERIAL PRIMARY KEY,
+    property_name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price FLOAT NOT NULL, 
+    availability_start DATE NOT NULL, 
+    availability_end DATE NOT NULL
+    );")
+
+  db.run("DROP TABLE IF EXISTS users;")
+  db.run("CREATE TABLE users (
+    id SERIAL PRIMARY KEY, 
+    username TEXT NOT NULL, 
+    password TEXT NOT NULL, 
+    contact TEXT NOT NULL, 
+    email TEXT NOT NULL,
+    );")
 end
 
 dev_db = DatabaseConnection.new("localhost", "web_application_dev")
